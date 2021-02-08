77778d1d0d07e6798051c8002982773d678f3086
Victor <viktorstankov@abv.bg>
2021-02-09 01:40:36 +0200

    Add new exercise

---
+++ Book/Chapter 3/3-6.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void itoa(int n, char s[], int min_width);
+void reverse(char s[]);
+
+int main()
+{
+    char number[100];
+    itoa(-123, number, 10);
+    return 0;
+}
+
+void itoa(int n, char s[], int min_width)
+{
+    int i, sign, negative_flag = 0;
+    if ((sign = n) < 0)
+        n = -n;
+    i = 0;
+    if(n < 0)  // Modified part
+    {
+        negative_flag = 1;
+        n += 1;
+        n = -n;
+    }
+    do {
+        if(negative_flag)  // Modified part
+        {
+            s[i++] = abs(n % 10) + '1';
+            negative_flag = 0;
+        }
+        else
+            s[i++] = abs(n % 10) + '0';
+    }
+    while ((n /= 10) > 0);
+    if (sign < 0)
+        s[i++] = '-';
+    if(i<min_width)
+        for(i; i<min_width; ++i)
+            s[i] = ' ';
+    s[i] = '\0';
+    reverse(s);
+    printf("%s\n", s);
+}
+
+void reverse(char s[])
+{
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--)
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
