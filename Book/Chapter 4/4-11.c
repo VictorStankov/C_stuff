ad3b1e464b123e941ce5e4321c57a4908bffbb06
VictorStankov <viktorstankov1@gmail.com>
2021-04-29 18:10:55 +0300

    Fixup commit because VSCode is a bit trash

--- Book/Chapter 4/4-11.c
+++ Book/Chapter 4/4-11.c
@@ -13,8 +13,6 @@
 int getop(char []);
 void push(double);
 double pop(void);
-// int getch(void);
-// void ungetch(int);
 void print();
 void duplicate();
 void swap();
@@ -180,21 +178,6 @@ int getop(char s[])
         return '-';
     return NUMBER;
 }
-
-// char buf[BUFSIZE]; /* buffer for ungetch */
-// int bufp = 0; /* next free position in buf */
-// int getch(void) /* get a (possibly pushed-back) character */
-// {
-//     return (bufp > 0) ? buf[--bufp] : getchar();
-// }
-
-// void ungetch(int c) /* push character back on input */
-// {
-//     if (bufp >= BUFSIZE)
-//         printf("ungetch: too many characters\n");
-//     else
-//         buf[bufp++] = c;
-// }
 
 void print()
 {
