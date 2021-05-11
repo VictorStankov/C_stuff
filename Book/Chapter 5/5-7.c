bf4b80be368b1e9a9e2ccba5bbb5287b9282a549
VictorStankov <viktorstankov1@gmail.com>
2021-05-11 16:38:05 +0300

    Fix copy-pasted code

--- Book/Chapter 5/5-7.c
+++ Book/Chapter 5/5-7.c
@@ -9,8 +9,9 @@ char *lineptr[MAXLINES]; /* pointers to text lines */
 int readlines(char *lineptr[], int nlines, char *buf[]);
 void writelines(char *lineptr[], int nlines);
 void qsort(char *lineptr[], int left, int right);
+
 /* sort input lines */
-main()
+int main()
 {
     /* 5-7. Rewrite readlines to store lines in an array supplied by main, rather than
        calling alloc to maintain storage. How much faster is the program?*/
@@ -20,7 +21,7 @@ main()
     {
         qsort(lineptr, 0, nlines-1);
         writelines(lineptr, nlines);
-    return 0;
+        return 0;
     }
     else
     {
