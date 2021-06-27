d62a14673fcfe7b97848fd18a0245fec96db5fce
VictorStankov <viktorstankov1@gmail.com>
2021-06-27 23:01:07 +0300

    Fix older exercises

--- Book/Chapter 1/1-20.c
+++ Book/Chapter 1/1-20.c
@@ -3,16 +3,27 @@
 
 int main()
 {
-    /* Write a program detab that replaces tabs in the input with the proper number
+    /* 1-20. Write a program detab that replaces tabs in the input with the proper number
        of blanks to space to the next tab stop. Assume a fixed set of tab stops, say every n columns.
        Should n be a variable or a symbolic parameter?*/
-    int c;
+    int c, letters = 0;
 
     while((c = getchar()) != EOF)
     {
+        letters++;
         if(c == '\t')
-        for(int i = 0; i<N; ++i)
-            putchar('a');
+        {
+            for(int i = 0; i <= N - letters % N; ++i)
+                putchar(' ');
+            letters = 0;
+        }
+        else if(c == '\n')
+        {
+            putchar(c);
+            letters = 0;
+        }
+        else
+            putchar(c);
     }
     return 0;
 }
--- Book/Chapter 1/1-21.c
+++ Book/Chapter 1/1-21.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#define N 4
 
 int main()
 {
-    /* Write a program entab that replaces strings of blanks by the minimum
+    /* 1-21. Write a program entab that replaces strings of blanks by the minimum
        number of tabs and blanks to achieve the same spacing. Use the same tab stops as for detab.
        When either a tab or a single blank would suffice to reach a tab stop, which should be given
        preference?*/
@@ -13,17 +14,21 @@ int main()
     {
         if(c == ' ')
             ++i;
-        if(i == 4)
+        if(i == N)
         {
             i = 0;
-            putchar('\t');
+            printf("aaaa");
+            continue;
         }
-        if(i > 0 && c == '\n')
+        if(i > 0 && c != ' ')
         {
-            for(int t = i % 4; t>0; --t)
-                putchar(' ');
+            for(int t = 0; t < i; ++t)
+                putchar('b');
             i = 0;
+            putchar(c);
         }
+        else if(c != ' ')
+            putchar(c);
     }
     return 0;
 }
