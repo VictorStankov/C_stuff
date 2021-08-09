bc763cac51710d42a7a90d045bcf56cef5ed0b86
VictorStankov <viktorstankov1@gmail.com>
2021-08-09 23:26:03 +0300

    Complete and tidy up the code

--- Book/Chapter 5/5-20.c
+++ Book/Chapter 5/5-20.c
@@ -1,133 +1,154 @@
-#include <stdio.h> 
-#include <string.h> 
-#include <ctype.h> 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 #define MAXTOKEN  100
 
-enum { NAME, PARENS, BRACKETS }; 
+enum { NAME, PARENS, BRACKETS };
 
-void dcl(void); 
-void dirdcl(void); 
+void dcl(void);
+void dirdcl(void);
 
 int bufp = 0; /* next free position in buf */
-int gettoken(void); 
-int tokentype;           /* type of last token */ 
-char token[MAXTOKEN];    /* last token string */ 
-char name[MAXTOKEN];     /* identifier name */ 
-char datatype[MAXTOKEN]; /* data type = char, int, etc. */ 
+int gettoken(void);
+int tokentype;           /* type of last token */
+char token[MAXTOKEN];    /* last token string */
+char name[MAXTOKEN];     /* identifier name */
+char datatype[MAXTOKEN]; /* data type = char, int, etc. */
 char out[1000];
-int const_flag = 0, name_flag = 0;  
+int const_flag = 0, name_flag = 0;
 
-int main()  /* convert declaration to words */ 
+int main()  /* convert declaration to words */
 {
     while (gettoken() != EOF) {   /* 1st token on line */
-		if(strcmp(token, "const") == 0)
-		{
-			const_flag = 1;
-			gettoken();
-		}
-        strcpy(datatype, token);  /* is the datatype */ 
-        out[0] = '\0'; 
-        dcl();       /* parse rest of line */ 
-		if(tokentype != '\n' || bufp != 0) 
-		{
-			printf("syntax error\n");
+        if(strcmp(token, "const") == 0)
+        {
+            const_flag = 1;
+            gettoken();
+        }
+        strcpy(datatype, token);  /* is the datatype */
+        out[0] = '\0';
+        dcl();       /* parse rest of line */
+        if(tokentype != '\n' || bufp != 0)
+        {
+            printf("syntax error\n");
             char c;
             while((c = getchar()) != '\n')
                 ;
             bufp = 0;
-		}
-		else
+        }
+        else
             printf("%s:%s%s %s\n", name, (const_flag) ? " constant" : "", out, datatype);
-    } 
-    return 0; 
+    }
+    return 0;
 }
 
-int gettoken(void)  /* return next token */ 
-{ 
-    int c, getch(void); 
-    void ungetch(int); 
-    char *p = token; 
+int gettoken(void)  /* return next token */
+{
+    int c, getch(void);
+    void ungetch(int);
+    char *p = token;
 
-    while ((c = getch()) == ' ' || c == '\t') 
+    while ((c = getch()) == ' ' || c == '\t')
         ;
 
-	if (c == '(') 
-	{ 
-        if ((c = getch()) == ')') { 
-            strcpy(token, "()"); 
-            return tokentype = PARENS; 
-        } else { 
-            ungetch(c); 
-            return tokentype = '('; 
-        } 
-    } 
-		
-	else if (c == '[') 
-	{ 
-        for (*p++ = c; (*p++ = getch()) != ']'; ) 
-            ; 
-        *p = '\0'; 
-        return tokentype = BRACKETS; 
-    } 
-	else if (isalpha(c)) 
-	{ 
-        for (*p++ = c; isalnum(c = getch()); ) 
-            *p++ = c; 
-        *p = '\0'; 
-        ungetch(c); 
-        return tokentype = NAME; 
-    } 
-	else 
-        return tokentype = c; 
+    if (c == '(')
+    {
+        if ((c = getch()) == ')') {
+            strcpy(token, "()");
+            return tokentype = PARENS;
+        } else {
+            ungetch(c);
+            return tokentype = '(';
+        }
+    }
+
+    else if (c == '[')
+    {
+        for (*p++ = c; (*p++ = getch()) != ']'; )
+            ;
+        *p = '\0';
+        return tokentype = BRACKETS;
+    }
+    else if (isalpha(c))
+    {
+        for (*p++ = c; isalnum(c = getch()); )
+            *p++ = c;
+        *p = '\0';
+        ungetch(c);
+        return tokentype = NAME;
+    }
+    else
+        return tokentype = c;
 }
 
-/* dcl:  parse a declarator */ 
-void dcl(void) 
-{ 
-    int ns; 
+/* dcl:  parse a declarator */
+void dcl(void)
+{
+    int ns;
 
     for (ns = 0; gettoken() == '*'; ) /* count *'s */
- 	            ns++; 
-    dirdcl(); 
-    while (ns-- > 0) 
-        strcat(out, " pointer to"); 
-} 
+        ns++;
+    dirdcl();
+    while (ns-- > 0)
+        strcat(out, " pointer to");
+}
 
-/* dirdcl:  parse a direct declarator */ 
-void dirdcl(void) 
+/* dirdcl:  parse a direct declarator */
+void dirdcl(void)
 {
-	int check_word(char *s);
-    int type; 
+    int check_word(char *s);
+    int type;
 
-	if (tokentype == '(') 
-	{         /* ( dcl ) */ 
-        dcl(); 
-        if (tokentype != ')') 
-            printf("error: missing )\n"); 
-    } 
-	else if (tokentype == NAME)  /* variable name */
-	{
-		if(check_word(token) == 0) // To finish
+    if (tokentype == '(')
+    {         /* ( dcl ) */
+        dcl();
+        if (tokentype != ')')
+            printf("error: missing )\n");
+    }
+    else if (tokentype == NAME)  /* variable name */
+    {
+        if(check_word(token) == 0) // To finish
         {
-			strcpy(name, token);
+            strcpy(name, token);
             name_flag = 1;
         }
-	}
+    }
 
-	else 
-        printf("error: expected name or (dcl)\n"); 
-    while ((type=gettoken()) == PARENS || type == BRACKETS || type == NAME) 
-		if (type == PARENS) 
+    else
+        printf("error: expected name or (dcl)\n");
+    while ((type=gettoken()) == PARENS || type == BRACKETS || type == NAME || type == '(')
+        if (type == PARENS)
             strcat(out, " function returning");
-		else if(type == NAME && name_flag == 0)
-			strcpy(name, token);
-		else 
-		{ 
-            strcat(out, " array"); 
-            strcat(out, token); 
-            strcat(out, " of"); 
-        } 
+        else if(type == NAME)
+        {
+            if(name_flag == 0)
+            {
+                strcpy(name, token);
+                name_flag = 1;
+            }
+            else if(check_word(token))
+            {
+                strcat(out, " function expecting ");
+                strcat(token, " ");
+                strcat(out, token);
+                while((type=gettoken()) != ')')
+                    if(check_word(token))
+                    {
+                        strcat(token, " ");
+                        strcat(out, token);
+                    }
+                strcat(out, "and returning");
+            }
+        }
+        else if(type == '(')
+            ;
+        else
+        {
+            strcat(out, " array");
+            strcat(out, token);
+            strcat(out, " of");
+        }
 }
 
 int getch() /* get a (possibly pushed-back) character */
@@ -145,19 +166,19 @@ void ungetch(int c) /* push character back on input */
         bufp = c;
 }
 
-int check_word(char *s) // To finish 
+int check_word(char *s) // To finish
 {
-	char words[][10] = {"void", "int", "double", "float", "char"};
-	if(strcmp(s, "const") == 0)
-	{
-		const_flag = 1;
-		return 1;
-	}
-	else
-	{
-		for(int i = 0; i < 5; ++i)
-			if(strcmp(s, words[i]) == 0)
-				return 1;
-	}
-	return 0;
+    char words[][10] = {"void", "int", "double", "float", "char"};
+    if(strcmp(s, "const") == 0)
+    {
+        const_flag = 1;
+        return 1;
+    }
+    else
+    {
+        for(int i = 0; i < 5; ++i)
+            if(strcmp(s, words[i]) == 0)
+                return 1;
+    }
+    return 0;
 }
